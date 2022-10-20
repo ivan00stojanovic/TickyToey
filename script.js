60d7fdc48511e556c25ba55f5a5a5342a2a816a3
meghann <meghannfh@gmail.com>
2022-10-20 15:22:36 -0600

    added check winner logic need to fix

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //randomly assign roles on pageload?
-let playerOne
-let playerTwo
-let playOneChoices
-let playerTwoChoices
+let playerOneChoices = []
+let playerTwoChoices = []
 let playerOneScore = 0;
 let playerTwoScore = 0;
 let drawScore = 0;
@@ -15,47 +13,73 @@ boxes.forEach((box) => {
         const turn = document.getElementById('turn')
         let target = e.target
         console.log(target.id)
-        if(clickCounter === 0){
-            target.innerText = 'X'
-        }else if(clickCounter % 2 === 0 && clickCounter !== 0){
-            target.innerText = 'X'
+        if(clickCounter % 2 === 0){
+            target.innerText = 'X'   
             turn.classList.remove('fa-x')
-            turn.classList.add('fa-o')    
+            turn.classList.add('fa-o')
+            playerOneChoices.push(Number(target.id))
+            
+
         }else{
             target.innerText = 'O'
             turn.classList.remove('fa-o')
             turn.classList.add('fa-x')
+            playerTwoChoices.push(Number(target.id))
+            
         }
         clickCounter++;
+        if(playerOneChoices.length  >= 3 || playerTwoChoices.length >= 3){
+            checkForWinner(playerOneChoices)
+            checkForWinner(playerTwoChoices)
+            console.log(`player one ${playerOneChoices}`)
+            console.log(`player two ${playerTwoChoices}`)
+        }/*else if(playerTwoChoices.length  === 3 || playerTwoChoices.length  === 4){
+            
+            
+        }*/
+        
         
     }, {once: true})
 })
 
-/* const switchTurn = (clickCounter) => {
-    const turn = document.getElementById('turn')
-    if(clickCounter % 2 === 0){
-        turn.classList.add('fa-x')
-        turn.classList.remove('fa-o')
-    }else{
-        turn.classList.remove('fa-x')
-        turn.classList.add('fa-o')
-    }
-} */
 
+
+function checkForWinner(arr) {
+    const winningPatterns = [
+        [1, 2, 3],
+        [1, 5, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 5, 7],
+        [3, 6, 9],
+        [4, 5, 6],
+    ]
+        let includeCounter = 0;
+
+        for(let i = 0; i < winningPatterns.length; i++){
+            for(let j = 0; i <= 2; j++){
+               if(arr.includes(winningPatterns[i][j])){
+                includeCounter++
+                console.log(`include counter is ${includeCounter}`)
+                if(includeCounter === 3){
+                    console.log('Winner!')
+                    alert('Winner')
+                }
+               }else{
+                includeCounter === 0  
+                    break;             
+               }
+            }
+        }
+        //[1,2.9].includes(winningPatterns[1][2])/
+  
+}
 
 //assign the first click to playerOne
 //assign second click to playerTwo
 //make sure to alternate between clicks;
 
 //winning patterns
-const winningPatterns = [
-    [1, 2, 3],
-    [1, 5, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 5, 7],
-    [3, 6, 9],
-    [4, 5, 6],
-]
+
 //player clicks a box and it pushes that number to an array
 
